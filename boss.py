cad045e99df4dbb6a44c4f34eaeda498e3c7169c
edwin2025-cyber <edwin.nunez-infante@ascte.org>
2025-09-08 09:08:24 -0500

    Create boss

---
+++ boss.py
@@ -0,0 +1,12 @@
+from enemy import Enemy
+
+class LLorona(Enemy):
+    def __init__(self, name):
+        super().__init__(name)
+        self.atack_power = 200
+
+    def laugh():
+        print("🤣🤣🤣")
+
+        
+
